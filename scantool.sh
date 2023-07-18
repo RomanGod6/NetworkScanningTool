ac5036f437cae5a4e9bc6bc00887d5bc1501da59
RomanGod6 <97071531+RomanGod6@users.noreply.github.com>
2023-07-18 15:13:46 -0400

    Update scantool.sh

--- scantool.sh
+++ scantool.sh
@@ -6,21 +6,12 @@ INTERFACES="eth3 eth4 eth5 eth6"
 for INTERFACE in $INTERFACES
 do
   echo "Scanning $INTERFACE ..."
-
+  
   # Run ARP command to retrieve the ARP table for the interface
   ARP_OUTPUT=$(arp)
   
-  # Print the ARP output for debugging
-  echo "ARP output: $ARP_OUTPUT"
-  
-  # Filter the ARP output for the current interface
-  INTERFACE_ARP_OUTPUT=$(echo "$ARP_OUTPUT" | grep $INTERFACE)
-  
-  # Print the filtered ARP output for debugging
-  echo "Filtered ARP output for $INTERFACE: $INTERFACE_ARP_OUTPUT"
-
   # Use a while-read loop to iterate over the lines of the ARP output
-  echo "$INTERFACE_ARP_OUTPUT" | while read -r line
+  echo "$ARP_OUTPUT" | while read -r line
   do
     # Extract the IP and MAC addresses from the line
     IP_ADDRESS=$(echo "$line" | awk '{print $1}')
