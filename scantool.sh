9b8f4af61cec8bc37fb057e4d77c0979e18e171f
RomanGod6 <97071531+RomanGod6@users.noreply.github.com>
2023-07-18 13:52:14 -0400

    Update scantool.sh

--- scantool.sh
+++ scantool.sh
@@ -6,7 +6,7 @@ INTERFACES="eth3 eth4 eth5 eth6"
 for INTERFACE in $INTERFACES
 do
   # Get the IP address and subnet mask for this interface
-  IP_AND_MASK=$(ifconfig $INTERFACE | awk '/inet / {print $2}')
+  IP_AND_MASK=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}')
   # If IP_AND_MASK is empty, skip this interface
   if [ -z "$IP_AND_MASK" ]; then
     echo "No IP address found for $INTERFACE"
