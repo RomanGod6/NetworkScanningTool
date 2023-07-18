f2753d51c1a41ea25d6f223cb766d1c2433a9be4
RomanGod6 <97071531+RomanGod6@users.noreply.github.com>
2023-07-18 13:31:30 -0400

    Update scantool.sh

--- scantool.sh
+++ scantool.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-# Ask the user for a port range to scan
-echo "Enter a port range to scan (for example, '1-1024'): "
-read PORT_RANGE
-# Get a list of all network interfaces
-INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
+# Specify the interfaces to scan
+INTERFACES=("eth3" "eth4" "eth5" "eth6")
+
 # Iterate over each interface
-for INTERFACE in $INTERFACES
+for INTERFACE in "${INTERFACES[@]}"
 do
   # Get the IP address and subnet mask for this interface
   IP_AND_MASK=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}')
   # If IP_AND_MASK is empty, skip this interface
   if [ -z "$IP_AND_MASK" ]; then
+    echo "No IP address found for $INTERFACE"
     continue
   fi
-  # Run a network scan on the specified port range and save the output to a file
-  nmap -p$PORT_RANGE -PR $IP_AND_MASK > "${INTERFACE}_scan.txt"
+  # Run a network scan and save the output to a file
+  nmap -sn -PR $IP_AND_MASK > "${INTERFACE}_scan.txt"
 done
