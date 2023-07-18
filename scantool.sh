85f6ac06cb5f17554862f4c8d872618d9df1bd52
RomanGod6 <97071531+RomanGod6@users.noreply.github.com>
2023-07-18 12:49:19 -0400

    Update scantool.sh

--- scantool.sh
+++ scantool.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Specify the interface to scan
-INTERFACE="eth0"
-# Get the IP address and subnet mask for this interface
-IP_AND_MASK=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}')
-# If IP_AND_MASK is empty, skip this interface
-if [ -z "$IP_AND_MASK" ]; then
-  echo "No IP address found for $INTERFACE"
-  exit 1
-fi
-# Run a network scan and save the output to a file
-nmap -sn -PR $IP_AND_MASK > "${INTERFACE}_scan.txt"
+# Get a list of all network interfaces
+INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
+# Iterate over each interface
+for INTERFACE in $INTERFACES
+do
+  # Get the IP address and subnet mask for this interface
+  IP_AND_MASK=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}')
+  # If IP_AND_MASK is empty, skip this interface
+  if [ -z "$IP_AND_MASK" ]; then
+    continue
+  fi
+  # Run a network scan and save the output to a file
+  nmap -sn -PR $IP_AND_MASK > "${INTERFACE}_scan.txt"
+done
